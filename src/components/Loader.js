c76779c0ac0e5480fb1ffe240ea00e71b5955fd1
Konrad Świć <kmswic@gmail.com>
2018-01-08 23:01:53 +0100

    Super mathematical refactoring

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,32 +2,22 @@ import * as React from 'react';
 
 export class Loader extends React.Component {
     render() {
+        const r = 100;
+        const midpoint = 250;
         return (
             <svg width={500} height={500}>
                 <circle r={100} cx={250} cy={250} fill="none" stroke="black" />
-                <circle class="petal" r={100} cx={336.6} cy={200} fill="none"
-                    stroke="black"
-                    stroke-dashoffset={-156}
-                />
-                <circle class="petal" r={100} cx={336.6} cy={300}
-                    fill="none" stroke="black" stroke-dashoffset={-263}
-                />
-                <circle class="petal" r={100} cx={250} cy={350} fill="none"
-                    stroke="black" stroke-dashoffset={-368}
-                />
-                <circle class="petal" r={100} cx={250} cy={150} fill="none"
-                    stroke="black"
-                    stroke-dashoffset={-52}
-                />
-
-                <circle class="petal" r={100} cx={163.3} cy={300} fill="none"
-                    stroke="black"
-                    stroke-dashoffset={-470}
-                />
-                <circle class="petal" r={100} cx={163.3} cy={200} fill="none"
-                    stroke="black"
-                    stroke-dashoffset={-600}
-                />
+                {[1, 2, 3, 4, 5, 6].map(i => {
+                    return (
+                        <circle
+                            r={r}
+                            cx={midpoint + Math.sin(i * Math.PI / 3) * r}
+                            cy={midpoint + Math.cos(i * Math.PI / 3) * r}
+                            fill="none"
+                            stroke="black"
+                        />
+                    )
+                })}
             </svg>
         )
     }
