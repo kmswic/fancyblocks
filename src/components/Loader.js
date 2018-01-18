9fecd12053606470aca8f2f2443b854e8fe8e0f8
Konrad Świć <kmswic@gmail.com>
2018-01-18 21:04:09 +0100

    remove magic constants

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -6,18 +6,16 @@ export class Loader extends React.Component {
         const midpoint = 250;
         return (
             <svg width={500} height={500}>
-                <circle r={100} cx={250} cy={250} fill="none" stroke="black" />
-                {[1, 2, 3, 4, 5, 6].map(i => {
-                    return (
-                        <circle
-                            r={r}
-                            cx={midpoint + Math.sin(i * Math.PI / 3) * r}
-                            cy={midpoint + Math.cos(i * Math.PI / 3) * r}
-                            fill="none"
-                            stroke="black"
-                        />
-                    )
-                })}
+                <circle r={r} cx={midpoint} cy={midpoint} fill="none" stroke="black" />
+                {[1, 2, 3, 4, 5, 6].map(i => (
+                    <circle
+                        r={r}
+                        cx={midpoint + Math.sin(i * Math.PI / 3) * r}
+                        cy={midpoint + Math.cos(i * Math.PI / 3) * r}
+                        fill="none"
+                        stroke="black"
+                    />
+                ))}
             </svg>
         )
     }
