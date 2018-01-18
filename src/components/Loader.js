af78ea9c3fe2807fb3d5a1b6233e0e1633504388
Konrad Świć <kmswic@gmail.com>
2018-01-18 21:17:53 +0100

    First interesting effect

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import './styles.css';
 
 export class Loader extends React.Component {
     render() {
         const r = 100;
         const midpoint = 250;
         return (
-            <svg width={500} height={500}>
-                <circle r={r} cx={midpoint} cy={midpoint} fill="none" stroke="black" />
+            <svg className="loader" width={500} height={500}>
+                <circle
+                    className="main"
+                    r={r}
+                    cx={midpoint}
+                    cy={midpoint}
+                    fill="none"
+                    stroke="black"
+                />
                 {[1, 2, 3, 4, 5, 6].map(i => (
                     <circle
+                        className={`petal petal-${i}`}
                         r={r}
                         cx={midpoint + Math.sin(i * Math.PI / 3) * r}
                         cy={midpoint + Math.cos(i * Math.PI / 3) * r}
