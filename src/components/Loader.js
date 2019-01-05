6dbc441a82fec30abcb2c26e27a98c6139192f60
Konrad Świć <kmswic@gmail.com>
2019-01-05 10:02:08 +0100

    Reworked after unexpected breakage

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,43 +1,42 @@
-import * as React from 'react';
-import './styles.css';
+import * as React from "react";
+import "./styles.css";
 
 export class Loader extends React.Component {
-    render() {
-        const r = 100;
-        const midpoint = 250;
-        const arc = 2 / 3 * Math.PI * r;
-        return (
-            <svg className="loader" width={500} height={500}>
-                <circle
-                    className="main"
-                    r={r}
-                    cx={midpoint}
-                    cy={midpoint}
-                    fill="none"
-                    stroke="black"
-                />
-                {[0, 1, 2, 3, 4, 5].map(i => (
-                    
-                    <circle
-                        className={`petal petal-${i}`}
-                        r={r}
-                        cx={midpoint + Math.sin(i * Math.PI / 3) * r}
-                        cy={midpoint + Math.cos(i * Math.PI / 3) * r}
-                        fill="none"
-                        style={{
-                            stroke: `hsl(${(i + 3) / 6 * 360}, 100%, 70%)`,
-                            // transformOrigin: '50% 50%',
-                            transform: `rotate(-${i * 60}deg)`
-                        }}
-                        strokeDasharray={`
+  render() {
+    const r = 100;
+    const midpoint = 250;
+    const arc = (2 / 3) * Math.PI * r;
+    return (
+      <svg className="loader" width={500} height={500}>
+        <circle
+          className="main"
+          r={r}
+          cx={midpoint}
+          cy={midpoint}
+          fill="none"
+          stroke="none"
+        />
+        {[0, 1, 2, 3, 4, 5].map(i => (
+          <circle
+            className={`petal petal-${i}`}
+            r={r}
+            cx={midpoint + Math.sin((1 * Math.PI) / 3) * r}
+            cy={midpoint + Math.cos((1 * Math.PI) / 3) * r}
+            fill="none"
+            style={{
+              stroke: `hsl(${(0 / 6) * 360}, 100%, 70%)`,
+              transformOrigin: "50% 50%",
+              transform: `rotate(-${i * 60}deg)`
+            }}
+            strokeDasharray={`
                             0
-                            ${arc * 7/4}
+                            ${(arc * 7) / 4}
                             ${arc}
-                            ${arc/4}
+                            ${arc / 4}
                         `}
-                    />
-                ))}
-            </svg>
-        )
-    }
+          />
+        ))}
+      </svg>
+    );
+  }
 }
